4975289f9cf0cb39243e9535eb778c0ef5dc2561
academicsociety2 <elembe2222@gmail.com>
2026-09-26 10:04:43 +0300

    Add files via upload

---
+++ Eto_Games/Data/data.js
@@ -0,0 +1,67 @@
+window.ETO_GAMES_PLATFORMS = [
+    {
+        id:'pc',
+        name:'PC',
+        icon:'fa-solid fa-desktop'
+    },
+    {
+        id:'steam',
+        name:'Steam',
+        icon:'fa-brands fa-steam'
+    },
+    {
+        id:'epic-games-store',
+        name:'Epic Games',
+        icon:'fa-solid fa-gamepad'
+    },
+    {
+        id:'gog',
+        name:'GOG',
+        icon:'fa-solid fa-cube'
+    },
+    {
+        id:'ps4',
+        name:'PlayStation 4',
+        icon:'fa-brands fa-playstation'
+    },
+    {
+        id:'ps5',
+        name:'PlayStation 5',
+        icon:'fa-brands fa-playstation'
+    },
+    {
+        id:'xbox-series-xs',
+        name:'Xbox Series X/S',
+        icon:'fa-brands fa-xbox'
+    },
+    {
+        id:'xbox-one',
+        name:'Xbox One',
+        icon:'fa-brands fa-xbox'
+    },
+    {
+        id:'switch',
+        name:'Nintendo Switch',
+        icon:'fa-solid fa-gamepad'
+    },
+    {
+        id:'android',
+        name:'Android',
+        icon:'fa-brands fa-android'
+    },
+    {
+        id:'ios',
+        name:'iOS',
+        icon:'fa-brands fa-apple'
+    },
+    {
+        id:'drm-free',
+        name:'DRM-Free',
+        icon:'fa-solid fa-unlock'
+    },
+    {
+        id:'itchio',
+        name:'itch.io',
+        icon:'fa-solid fa-ghost'
+    }
+];
